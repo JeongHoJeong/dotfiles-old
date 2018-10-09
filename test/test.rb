5ba4156dfc1737e1f80b749a4b44ab08fb96cdb6
Jeongho Jeong <fiil12@hotmail.com>
2018-10-09 18:58:59 +0900

    save

---
+++ test/test.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'dotfiles/manage.rb'
+
+RSpec.describe "fib" do
+  it "has to do" do
+    expect(Dotfiles::fib(0)).to eq(0)
+    expect(Dotfiles::fib(1)).to eq(1)
+    expect(Dotfiles::fib(2)).to eq(1)
+    expect(Dotfiles::fib(3)).to eq(2)
+  end
+end
